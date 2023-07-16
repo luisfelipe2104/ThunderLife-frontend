47d4277ff16e7ae24ba5f87a35dcb5de9fd16ff2
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-07-16 18:42:33 -0300

    building the login screen

--- components/AuthComponents.js
+++ components/AuthComponents.js
@@ -1,4 +1,6 @@
 import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+const screenHeight = Dimensions.get('window').height
 
 export const Container = styled.View`
     background-color: #2b2b2b;
@@ -7,13 +9,16 @@ export const Container = styled.View`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-between;
+    /* justify-content: space-between; */
 `
 
 export const WelcomeContainer = styled.View`
     display: flex;
     flex-direction: column;
     align-items: center;
+    height: 47%;
+
+    /* border: 2px solid red; */
 `
 
 export const LogoImage = styled.Image`
@@ -38,11 +43,13 @@ export const ImpactPhrase = styled.Text`
 `
 
 export const ButtonContainer = styled.View`
-    height: 130px;
-    margin-top: 20px;
+    height: 30%;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    /* justify-content: space-between; */
+
+    /* border: 2px solid blue; */
+    padding-top: 25px;
 `
 
 export const LoginButton = styled.TouchableOpacity`
@@ -56,6 +63,7 @@ export const RegisterButton = styled.TouchableOpacity`
     padding: 3px 3px;
     border-radius: 15px;
     border: 3px solid #edea0c;
+    margin-top: 30px;
 `
 
 export const LoginButtonText = styled.Text`
@@ -78,9 +86,63 @@ export const RegisterButtonText = styled.Text`
 `
 
 export const FooterContainer = styled.View`
-
+    height: 23%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    /* border: 2px solid green; */
 `
 
 export const WavesImage = styled.Image`
     height: 180px;
+`
+
+export const InputContainer = styled.View`
+    width: 100%;
+    height: 57%;
+    /* border: 2px solid red; */
+    
+`
+
+export const AuthNavigationText = styled.Text`
+    color: #FFF;
+    font-weight: 600;
+    font-size: 18px;
+    padding: 0 10%;
+`
+
+export const KeyboardAvoidingView = styled.KeyboardAvoidingView`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100px;
+    justify-content: space-between;
+    margin-top: 20%;
+`
+
+export const Input = styled.TextInput`
+    width: 80%;
+    padding: 3px 5px 3px 7px;
+    color: #FFF;
+    font-size: 19px;
+    background-color: #454545;
+    margin-bottom: 10px;
+    border-bottom-width: 2px;
+    border-bottom-color: #edea0c;
+    border-radius: 8px;
+`
+
+export const TitleContainer = styled.View`
+    height: 20%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const AuthTitle = styled.Text`
+    color: #FFF;
+    font-size: 35px;
+    font-weight: 800;
+    margin-bottom: 10px;
 `
--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import { Container, KeyboardAvoidingView, ImpactPhrase, AuthNavigationText, AuthTitle, WavesImage, FooterContainer, TitleContainer, InputContainer, Input } from '../../components/AuthComponents'
+
+import waves from '../../assets/waves.png'
 
 export default function Login() {
   return (
-    <View>
-        <Text>Login</Text>
-    </View>
+    <Container>
+        <TitleContainer>
+          <AuthTitle>⚡Login⚡</AuthTitle>
+          <ImpactPhrase>"Success is... TRAINING"</ImpactPhrase>
+        </TitleContainer>
+
+        <InputContainer>
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <Input 
+              placeholder='Email'
+              placeholderTextColor="#858585"  
+            />
+            <Input 
+              placeholder='Password'
+              placeholderTextColor="#858585"
+            />
+          </KeyboardAvoidingView>
+          <AuthNavigationText>Already have an account?</AuthNavigationText>
+        </InputContainer>
+
+        <FooterContainer>
+          <WavesImage source={waves} />
+        </FooterContainer>
+    </Container>
   )
 }
